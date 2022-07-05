f2dcb249622c30b43aa015a3e062a8989401966d
xiaojingming <2413294257@qq.com>
2022-07-05 17:53:56 +0800

    blob-docs change

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,11 @@
 # 确保脚本抛出遇到的错误
 set -e
 
+# 添加到docs仓库
+git add .
+git commit -m 'blob-docs change'
+git push origin master git@github.com:xiaojingming/my-blob-docs.git
+
 # 生成静态文件
 npm run docs:build
 
@@ -14,6 +19,6 @@ git add -A
 git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:xiaojingming/my-blob.git master:gh-pages
+git push -f build git@github.com:xiaojingming/my-blob.git master:gh-pages
 
 cd -
