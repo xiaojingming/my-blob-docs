ded6effa0f55962bc38b76f6cc9c36990f457ebb
xiaojingming <2413294257@qq.com>
2022-07-06 10:06:45 +0800

    blob-docs change

--- docs/.vuepress/deploy.sh
+++ docs/.vuepress/deploy.sh
@@ -4,17 +4,18 @@
 set -e
 
 # 添加到docs仓库
-git add .
-git commit -m 'blob-docs change'
-git pull origin master
-git push -u origin master
+# git add .
+# git commit -m 'blob-docs change'
+# git pull origin master
+# git push -u origin master
 
 # 生成静态文件
 npm run docs:build
 
 # # 进入生成的文件夹 
 cd docs/.vuepress/dist
-
+git init
+git remote add build git@github.com:xiaojingming/my-blob.git
 git add -A
 git commit -m 'deploy'
 git pull build master --allow-unrelated-histories
