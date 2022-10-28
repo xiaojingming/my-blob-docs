038dede8ec077485d4e8dbe9432baf2fba9c94ff
xiaojingming <2413294257@qq.com>
2022-10-28 17:02:48 +0800

    blob-docs change

--- docs/.vuepress/enhanceApp.js
+++ docs/.vuepress/enhanceApp.js
@@ -1,5 +1,10 @@
 export default ({ router }) => {
   router.beforeEach((to, from, next) => {
+    try {
+      console.log(_hmt, 'hmt-');
+    } catch (error) {
+      console.log('error', error)
+    }
     if (typeof _hmt !== "undefined") {
       if (to.path) {
         _hmt.push(["_trackPageview", to.fullPath]);
