ec9481d990ce4ff02e13cfa45dc2bf93d1370123
xiaojingming <2413294257@qq.com>
2022-10-28 16:17:16 +0800

    blob-docs change

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -11,6 +11,16 @@ module.exports = {
   },
   head: [
     [ 'link', { rel: 'icon', href: '/icon.png' } ],
+    [ 'script', {}, `
+    var _hmt = _hmt || [];
+    (function() {
+      var hm = document.createElement("script");
+      hm.src = "https://hm.baidu.com/hm.js?453068cd0bccee05b7686aa463d0bc60";
+      var s = document.getElementsByTagName("script")[0]; 
+      s.parentNode.insertBefore(hm, s);
+    })();
+    </script>
+    `],
   ],
   themeConfig: {
     nav: [
